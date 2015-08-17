41cd82416e5f315734d5388ecf46fd3940d0ba8d
absentees <scott@presentcompany.co>
2015-08-17 20:17:45 +1000

    Bail on ember and socket io

--- seeburg-server/app.js
+++
@@ -1,9 +0,0 @@
-var express = require(express);
-var socketio = require('socket.io');
-var app = express();
-
-var io = socketio.listen(app.listen(3000));
-
-io.sockets.on('connection', function (socket) {
-  console.log('A user has connected');
-});
