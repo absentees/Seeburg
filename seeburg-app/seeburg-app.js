bd5aaa8f4ba4636c6a01362f8343f2a6a2ca2747
absentees <scottblissett@gmail.com>
2015-08-25 00:01:35 +1000

    request code mech

--- seeburg-app/seeburg-app.js
+++ seeburg-app/seeburg-app.js
@@ -1,1 +1,13 @@
-RoomsList = new Mongo.Collection('rooms');
+InviteKeys = new Meteor.Collection('invitekeys');
+Rooms = new Meteor.Collection('rooms');
+Tracks = new Meteor.Collection('tracks');
+
+Router.configure({
+  layoutTemplate: 'main',
+  loadingTemplate: 'loading'
+});
+
+Router.route('/', {
+  name: 'home',
+  template: 'home'
+});
