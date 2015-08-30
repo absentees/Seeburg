26af79bb5eec0502505ece09ed2ee32a4ecc2c0f
absentees <scottblissett@gmail.com>
2015-08-30 19:22:21 +1000

    yew

--- seeburg-app/client/client.js
+++ seeburg-app/client/client.js
@@ -21,17 +21,61 @@ Template.home.events({
   }
 });
 
+Template.login.events({
+  'submit form': function(event){
+    event.preventDefault();
+  }
+});
+
+Template.login.onRendered(function(){
+  var validator = $('.login').validate({
+    submitHandler: function(event){
+      var email = $('[name=email]').val();
+      var password = $('[name=password]').val();
+      Meteor.loginWithPassword(email,password, function(err){
+        if(err){
+
+          if (err.reason == "User not found") {
+            validator.showErrors({
+              email: "that email does not exist to a registered user."
+            })
+          }
+          if (err.reason == "Incorrect password") {
+            validator.showErrors({
+              password: "you entered an incorrect password."
+            })
+          }
+        } else{
+          var currentRouter = Router.current().route.getName();
+          if (currentRoute == "login") {
+            Router.go("home");
+          }
+        }
+      })
+    }
+  })
+});
+
 $.validator.setDefaults({
   rules: {
     email: {
       required: true,
       email: true
+    },
+    password: {
+      required: true,
+      minlength: 6
     }
   },
   messages: {
     email: {
       required: "you must enter an email address",
       email: "you have entered an invalid email"
+    },
+    password: {
+      required: "you must enter a password.",
+      minlength: "your password must be at least {0} characters."
     }
   }
+
 });
