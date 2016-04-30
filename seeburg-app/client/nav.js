6e463bceb8ebc0e4c98102ceb8147e510611af04
absentees <scott@presentcompany.co>
2016-04-30 12:15:46 +1000

    Header nav

--- seeburg-app/client/nav.js
+++ seeburg-app/client/nav.js
@@ -1,7 +1,12 @@
-Template.navigation.events({
+Template.loginRegister.events({
   'click .logout': function(event) {
     event.preventDefault();
     Meteor.logout();
     Router.go('login');
+  },
+  'click .login': function(event){
+    event.preventDefault();
+    $('.loginRegisterForm').toggleClass('-active');
+    $('.header-bar').toggleClass('-active');
   }
 });
